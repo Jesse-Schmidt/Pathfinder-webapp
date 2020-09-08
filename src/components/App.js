e1933c75dfafaaa23f9a1ee27850f1cad759e62d
Jesse Schmidt <schmidt.j.jesse@gmail.com>
2020-09-08 12:41:18 -0600

    add header functionality

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
-
+import logo from './images/pathfinder.png';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img className={"Image"} src={logo} alt={"Pathfinder Roleplaying Game"}/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
