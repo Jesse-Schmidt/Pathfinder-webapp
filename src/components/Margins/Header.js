e35e3454108e298791d122856dd10053f7f132bd
Jesse Schmidt <schmidt.j.jesse@gmail.com>
2020-09-09 12:35:08 -0600

    add logo spacing

--- src/components/Margins/Header.js
+++ src/components/Margins/Header.js
@@ -1,39 +1,26 @@
 import React, {Component} from 'react';
 import {Container} from "reactstrap";
-import Select from "@material-ui/core/Select";
-import FormControl from "@material-ui/core/FormControl"
-import MenuItem from '@material-ui/core/MenuItem';
-
+import {renderDropdown} from '../../utils/HelpfulFunctions.js'
 
 import './header-footer.css';
-import logo from'./resources/pathfinder.png'
+import logo from './resources/pathfinder.png'
+import spacing from './resources/spacing.png'
 
 export default class Header extends Component {
 
     render() {
         return (
             <div className="full-width header">
-                <div className="vertical-center">
-                    <Container>
-                        <div className="vertical-center">
-                            <img className="logo" src={logo} alt="Pathfinder Role Playing Games"/>
-                        </div>
-                        <div className="page-select">
-                            <FormControl style={{margin:"auto"}}>
-                                <Select
-                                    labelId="demo-simple-select-label"
-                                    id="demo-simple-select"
-                                    value={this.props.selectedPage}
-                                    onChange={this.props.togglePages}
-                                >
-                                    <MenuItem value={"Basic Info"}>Basic Info</MenuItem>
-                                    <MenuItem value={"Equipment"}>Equipment</MenuItem>
-                                    <MenuItem value={"Combat"}>Combat</MenuItem>
-                                </Select>
-                            </FormControl>
-                        </div>
-                    </Container>
-                </div>
+                <Container>
+                    <div className="vertical-center">
+                        <img className="spacer" src={spacing} alt="cool red line" />
+                        <img className="logo" src={logo} alt="Pathfinder Role Playing Games"/>
+                        <img className="spacer" src={spacing} alt="cool red line" />
+                    </div>
+                    <div className="page-select">
+                        {renderDropdown("pageSelect", this.props.selectedPage, this.props.togglePages, this.props.pageList)}
+                    </div>
+                </Container>
             </div>
         );
     }
